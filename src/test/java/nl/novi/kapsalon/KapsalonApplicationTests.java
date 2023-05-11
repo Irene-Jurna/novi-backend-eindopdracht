17befbcd387b27d226005233a5c34f6aca3ec694
Irene Jurna <irene.jurna@gmail.com>
2023-05-11 15:10:33 +0200

    start eindproject

---
+++ src/test/java/nl/novi/kapsalon/KapsalonApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.kapsalon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KapsalonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
