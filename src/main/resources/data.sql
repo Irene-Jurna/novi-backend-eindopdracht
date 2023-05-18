1110e1bfc811142d273c77b1c60ba20f66874362
Irene Jurna <irene.jurna@gmail.com>
2023-05-18 15:40:16 +0200

    Data.sql alle behandelingen toegevoegd

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,14 @@
 INSERT INTO users (id, first_name, last_name, email, password, address, house_number, residence, in_case_of_emergency_contact, emergency_contact_phone_number, preferred_hairdresser, notes) VALUES (0, 'Marianne', 'Timmer', 'marie@hotmail.com', 'liefdevoorschaatsen123', 'Lagestraat', 1, 'Stedum', 'Anna Peters', '06123456', '', '');
+
+INSERT INTO treatments (id, name, duration_in_minutes, price) VALUES (0, 'Knippen dames en heren', 25, 25);
+INSERT INTO treatments (id, name, duration_in_minutes, price) VALUES (1, 'Knippen kinderen', 25, 23.50);
+INSERT INTO treatments (id, name, duration_in_minutes, price) VALUES (2, 'Knippen minderharigen', 20, 20);
+INSERT INTO treatments (id, name, duration_in_minutes, price) VALUES (3, 'Knippen wassen', 35, 33.50);
+INSERT INTO treatments (id, name, duration_in_minutes, price) VALUES (4, 'Knippen coupe soleil', 150, 80);
+INSERT INTO treatments (id, name, duration_in_minutes, price) VALUES (5, 'Knippen wassen watergolven', 120, 45);
+INSERT INTO treatments (id, name, duration_in_minutes, price) VALUES (6, 'Omvorming', 160, 95);
+INSERT INTO treatments (id, name, duration_in_minutes, price) VALUES (7, 'Omvorming lang haar', 180, 137.50);
+INSERT INTO treatments (id, name, duration_in_minutes, price) VALUES (8, 'Verven vanaf', 60, 50);
+INSERT INTO treatments (id, name, duration_in_minutes, price) VALUES (9, 'Spoeling vanaf', 50, 48);
+INSERT INTO treatments (id, name, duration_in_minutes, price) VALUES (10, 'Wassen watergolven', 95, 33.50);
+INSERT INTO treatments (id, name, duration_in_minutes, price) VALUES (11, 'Vlechten vanaf', 30, 28.50);
