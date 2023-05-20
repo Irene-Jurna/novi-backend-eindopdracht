a0c8308517817ca6e093bb7ef68487631205a577
Irene Jurna <irene.jurna@gmail.com>
2023-05-20 11:55:44 +0200

    CalculateCombinedDuration werkt

--- src/main/java/nl/novi/kapsalon/services/TreatmentService.java
+++ src/main/java/nl/novi/kapsalon/services/TreatmentService.java
@@ -7,7 +7,6 @@ import nl.novi.kapsalon.repositories.TreatmentRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
