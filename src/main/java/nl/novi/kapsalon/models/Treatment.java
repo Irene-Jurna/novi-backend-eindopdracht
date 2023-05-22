ad7c9175d71c57f8238c77267dc28bcb3ef745ad
Irene <irene.jurna@gmail.com>
2023-05-22 18:34:58 +0200

    BaseModel toegevoegd

--- src/main/java/nl/novi/kapsalon/models/Treatment.java
+++ src/main/java/nl/novi/kapsalon/models/Treatment.java
@@ -1,47 +1,19 @@
 package nl.novi.kapsalon.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 @Entity
 @Table(name="treatments")
-public class Treatment {
+public class Treatment extends BaseModel {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
     private String name;
     private Integer durationInMinutes;
     private Double price;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Integer getDurationInMinutes() {
-        return durationInMinutes;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDurationInMinutes(Integer durationInMinutes) {
-        this.durationInMinutes = durationInMinutes;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
 }
--- src/main/java/nl/novi/kapsalon/repositories/TreatmentRepository.java
+++ src/main/java/nl/novi/kapsalon/repositories/TreatmentRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface TreatmentRepository extends JpaRepository<Treatment, Integer> {
-    List<Treatment> findAllByIdIn(List<Integer> treatmentIds);
+public interface TreatmentRepository extends JpaRepository<Treatment, Long> {
+    List<Treatment> findAllByIdIn(List<Long> treatmentIds);
 }
