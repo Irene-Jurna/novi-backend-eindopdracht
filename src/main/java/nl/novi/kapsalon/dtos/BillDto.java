7d03b505d0c5778cd8720842438a586c08a7e675
Irene <irene.jurna@gmail.com>
2023-05-27 16:53:05 +0200

    Pogingen met relaties, niet gelukt

--- src/main/java/nl/novi/kapsalon/dtos/BillDto.java
+++ src/main/java/nl/novi/kapsalon/dtos/BillDto.java
@@ -1,15 +1,60 @@
 package nl.novi.kapsalon.dtos;
 
-import nl.novi.kapsalon.models.Product;
-import nl.novi.kapsalon.models.Treatment;
-
 import java.util.List;
 
 public class BillDto {
     public Long id;
     public Long customerId;
     public Long hairdresserId;
-    public List<Treatment> treatments;
-    public List<Product> products;
+    public List<Long> treatments;
+    public List<Long> products;
     public Boolean paid;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public Long getHairdresserId() {
+        return hairdresserId;
+    }
+
+    public List<Long> getTreatments() {
+        return treatments;
+    }
+
+    public List<Long> getProducts() {
+        return products;
+    }
+
+    public Boolean getPaid() {
+        return paid;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public void setHairdresserId(Long hairdresserId) {
+        this.hairdresserId = hairdresserId;
+    }
+
+    public void setTreatments(List<Long> treatments) {
+        this.treatments = treatments;
+    }
+
+    public void setProducts(List<Long> products) {
+        this.products = products;
+    }
+
+    public void setPaid(Boolean paid) {
+        this.paid = paid;
+    }
 }
