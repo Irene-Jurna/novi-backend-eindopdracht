bd4aa503fa240db4de6d24146facb5d6e1547e2c
Irene <irene.jurna@gmail.com>
2023-06-07 10:42:34 +0200

    Inloggen werkt, token wordt gegenereerd

--- src/main/java/nl/novi/kapsalon/repositories/UserRepository.java
+++ src/main/java/nl/novi/kapsalon/repositories/UserRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query("Select u from User u where lower (u.firstName) = lower(:firstname) and lower (u.lastName) = lower(:lastname)")
     User findUsersByFirstNameAndLastName(@Param("firstname") String firstName, @Param("lastname") String lastName);
+    Optional<User> findUserByUsername(@Param("username") String username);
 
     List<User> findUsersByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(String firstNameSubString, String lastNameSubString);
 }
