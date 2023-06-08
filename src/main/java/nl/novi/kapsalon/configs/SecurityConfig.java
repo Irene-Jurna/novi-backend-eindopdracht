78bfb69db797feef368494d8d2ff5353d3932792
Irene <irene.jurna@gmail.com>
2023-06-08 10:35:33 +0200

    Werkuurtje met Jasper

--- src/main/java/nl/novi/kapsalon/configs/SecurityConfig.java
+++ src/main/java/nl/novi/kapsalon/configs/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
         http
                 .httpBasic().disable()
                 .authorizeHttpRequests()
-                .requestMatchers(HttpMethod.GET, "/agenda").permitAll()
+                .requestMatchers(HttpMethod.GET, "/agenda").hasAuthority("Customer")
                 .requestMatchers(HttpMethod.POST, "/agenda").hasAnyAuthority("Hairdresser", "Owner")
                 .requestMatchers(HttpMethod.PUT, "/agenda/**").hasAnyAuthority("Hairdresser", "Owner")
                 .requestMatchers(HttpMethod.DELETE, "/agenda/**").hasAnyAuthority("Hairdresser", "Owner")
--- src/main/java/nl/novi/kapsalon/controllers/UserController.java
+++ src/main/java/nl/novi/kapsalon/controllers/UserController.java
@@ -5,6 +5,7 @@ import nl.novi.kapsalon.dtos.UserDto;
 import nl.novi.kapsalon.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
@@ -16,10 +17,10 @@ import java.util.List;
 @RestController
 @RequestMapping("users")
 public class UserController {
-    private final UserService userservice;
+    private final UserService userService;
 
-    public UserController(UserService userservice) {
-        this.userservice = userservice;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @PostMapping("")
@@ -34,7 +35,7 @@ public class UserController {
             return new ResponseEntity<>(sb.toString(), HttpStatus.BAD_REQUEST);
         }
 
-        Long id = userservice.createUser(userDto);
+        Long id = userService.createUser(userDto);
         userDto.id = id;
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentRequest().path("/" + id).toUriString());
         return ResponseEntity.created(uri).body(userDto);
@@ -42,31 +43,32 @@ public class UserController {
 
     @GetMapping("")
     public ResponseEntity<List<UserDto>> getAllUsers() {
-        List<UserDto> dtoList = userservice.getAllUsers();
+        List<UserDto> dtoList = userService.getAllUsers();
         return ResponseEntity.ok(dtoList);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
-        UserDto userDto = userservice.getUser(id);
+        UserDto userDto = userService.getUser(id);
         return ResponseEntity.ok(userDto);
     }
 
+    @PreAuthorize("hasR('deleteUser')")
     @GetMapping("search")
     public ResponseEntity<List<UserDto>> getUsersBasedOnSubString(@RequestParam String subString) {
-        List<UserDto> usersWithSubstring = userservice.getUsersBasedOnSubString(subString, subString);
+        List<UserDto> usersWithSubstring = userService.getUsersBasedOnSubString(subString, subString);
         return ResponseEntity.ok(usersWithSubstring);
     }
 
     @PutMapping("{id}")
     public ResponseEntity<UserDto> updateUser(@Valid @PathVariable("id") Long id, @RequestBody UserDto userDto) {
-        userservice.updateUser(id, userDto);
+        userService.updateUser(id, userDto);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteUser(@PathVariable Long id) {
-        userservice.deleteUser(id);
+        userService.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
 
