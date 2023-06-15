a80831ec4909153b001e1ebb62f1e3aeea08284c
Irene <irene.jurna@gmail.com>
2023-06-15 19:44:47 +0200

    Laatste security toegevoegd: AgendaController

--- src/main/java/nl/novi/kapsalon/controllers/AgendaController.java
+++ src/main/java/nl/novi/kapsalon/controllers/AgendaController.java
@@ -5,6 +5,7 @@ import nl.novi.kapsalon.dtos.AgendaDto;
 import nl.novi.kapsalon.services.AgendaService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,24 +19,28 @@ public class AgendaController {
         this.agendaService = agendaService;
     }
 
+    @PreAuthorize("hasRole ('ROLE_OWNER')")
     @PostMapping("")
     public ResponseEntity<Object> createAgenda(@Valid @RequestBody AgendaDto aDto) {
         agendaService.createAgenda(aDto);
         return new ResponseEntity<>(aDto, HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_HAIRDRESSER', 'ROLE_OWNER')")
     @GetMapping("")
     public ResponseEntity<List<AgendaDto>> getAllAgendas() {
         List<AgendaDto> dtoList = agendaService.getAllAgendas();
         return ResponseEntity.ok(dtoList);
     }
 
+    @PreAuthorize("hasRole ('ROLE_OWNER')")
     @PutMapping("{id}")
     public ResponseEntity<AgendaDto> updateAgenda(@Valid @PathVariable("id") Long id, @RequestBody AgendaDto agendaDto) {
         agendaService.updateAgenda(id, agendaDto);
         return new ResponseEntity<>(agendaDto, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole ('ROLE_OWNER')")
     @DeleteMapping("{id}")
     public ResponseEntity<Object> deleteAgenda(@PathVariable("id") Long id) {
         agendaService.deleteAgenda(id);
