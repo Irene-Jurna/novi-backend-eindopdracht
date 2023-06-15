ce8d608123855591c87147a6bb00cff940f152c3
Irene <irene.jurna@gmail.com>
2023-06-15 18:17:16 +0200

    Security TreatmentController werkt

--- src/main/java/nl/novi/kapsalon/controllers/TreatmentController.java
+++ src/main/java/nl/novi/kapsalon/controllers/TreatmentController.java
@@ -2,10 +2,10 @@ package nl.novi.kapsalon.controllers;
 
 import jakarta.validation.Valid;
 import nl.novi.kapsalon.dtos.TreatmentDto;
-import nl.novi.kapsalon.dtos.UserDto;
 import nl.novi.kapsalon.services.TreatmentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,30 +19,35 @@ public class TreatmentController {
         this.treatmentService = treatmentService;
     }
 
+    @PreAuthorize("hasRole('ROLE_OWNER')")
     @PostMapping("")
     public ResponseEntity<Object> createTreatment(@Valid @RequestBody TreatmentDto tdto) {
         treatmentService.createTreatment(tdto);
         return new ResponseEntity<>(tdto, HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_HAIRDRESSER', 'ROLE_OWNER')")
     @GetMapping("")
     public ResponseEntity<List<TreatmentDto>> getAllTreatments() {
         List<TreatmentDto> dtoList = treatmentService.getAllTreatments();
         return ResponseEntity.ok(dtoList);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_HAIRDRESSER', 'ROLE_OWNER')")
     @GetMapping("/sum")
     public ResponseEntity<Integer> calculateCombinedDuration(@RequestBody List<Long> treatmentIds) {
         int combinedDuration = treatmentService.calculateCombinedDuration(treatmentIds);
         return new ResponseEntity<>(combinedDuration, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ROLE_OWNER')")
     @PutMapping("{id}")
     public ResponseEntity<TreatmentDto> updateTreatment(@Valid @PathVariable("id") Long id, @RequestBody TreatmentDto treatmentDto) {
         treatmentService.updateTreatment(id, treatmentDto);
         return new ResponseEntity<>(treatmentDto, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ROLE_OWNER')")
     @DeleteMapping("{id}")
     public ResponseEntity<Object> deleteTreatment(@PathVariable("id") Long id) {
         treatmentService.deleteTreatment(id);
