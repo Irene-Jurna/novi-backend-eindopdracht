5832d1558145d7d477a0ca264ea16a2b7b13b68f
Irene <irene.jurna@gmail.com>
2023-06-16 12:58:29 +0200

    BillOutputDto toegevoegd die het eindbedrag op de rekening retourneert

--- src/main/java/nl/novi/kapsalon/controllers/BillController.java
+++ src/main/java/nl/novi/kapsalon/controllers/BillController.java
@@ -2,6 +2,7 @@ package nl.novi.kapsalon.controllers;
 
 import jakarta.validation.Valid;
 import nl.novi.kapsalon.dtos.BillDto;
+import nl.novi.kapsalon.dtos.BillOutputDto;
 import nl.novi.kapsalon.services.BillService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,9 +20,9 @@ public class BillController {
     }
 
     @PostMapping("")
-    public ResponseEntity<Object> createBill(@Valid @RequestBody BillDto bDto) {
-        billService.createBill(bDto);
-        return new ResponseEntity<>(bDto, HttpStatus.CREATED);
+    public ResponseEntity<BillOutputDto> createBill(@Valid @RequestBody BillDto bDto) {
+        BillOutputDto boDto = billService.createBill(bDto);
+        return new ResponseEntity<>(boDto, HttpStatus.CREATED);
     }
 
     @GetMapping("{id}")
---
+++ src/main/java/nl/novi/kapsalon/dtos/BillOutputDto.java
@@ -0,0 +1,13 @@
+package nl.novi.kapsalon.dtos;
+
+public class BillOutputDto {
+    public double totalPrice;
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
