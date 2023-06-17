b489ded333a73f5470affac28659a230d4bfbf91
Irene <irene.jurna@gmail.com>
2023-06-17 11:16:05 +0200

    Eerste unit test

--- src/main/java/nl/novi/kapsalon/models/User.java
+++ src/main/java/nl/novi/kapsalon/models/User.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.time.Instant;
+
 @EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
---
+++ src/test/java/nl/novi/kapsalon/models/UserTest.java
@@ -0,0 +1,21 @@
+package nl.novi.kapsalon.models;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    @DisplayName("Should return correct firstname")
+    void getFirstName() {
+        User user = new User("Mark", "Rensen", "markrensen", "markrensen@novi-education.nl", "novi12345", "Newtonlaan", 3, "Utrecht",
+                "Nova Eeken", "0612345678", "Ellen", "", null);
+
+        String result = user.getFirstName();
+
+        assertEquals("Mark", result);
+    }
+
+}
