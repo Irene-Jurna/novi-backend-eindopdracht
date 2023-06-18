48a540860f6bf4cf6dc7cfefbd423eddbf85d16b
Irene <irene.jurna@gmail.com>
2023-06-18 10:37:39 +0200

    Aanpassingen BillControllerTest met document testen van je code 2.1, werkt nog niet

--- src/test/java/nl/novi/kapsalon/models/UserTest.java
+++ src/test/java/nl/novi/kapsalon/models/UserTest.java
@@ -2,6 +2,8 @@ package nl.novi.kapsalon.models;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
