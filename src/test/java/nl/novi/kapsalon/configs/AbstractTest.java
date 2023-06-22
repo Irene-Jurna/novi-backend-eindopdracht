6609636d6b1fefa75117b0d5660907128b2ca158
Irene <irene.jurna@gmail.com>
2023-06-22 18:49:41 +0200

    Test deleteBill aangescherpt en geslaagd

--- src/test/java/nl/novi/kapsalon/configs/AbstractTest.java
+++ src/test/java/nl/novi/kapsalon/configs/AbstractTest.java
@@ -3,17 +3,15 @@ package nl.novi.kapsalon.configs;
 import nl.novi.kapsalon.repositories.UserRepository;
 import nl.novi.kapsalon.services.JwtService;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @Import(SecurityConfig.class)
 @ExtendWith(SpringExtension.class)
-public class AbstractTest {
+public abstract class AbstractTest {
 
     @Autowired
     protected MockMvc mockMvc;
