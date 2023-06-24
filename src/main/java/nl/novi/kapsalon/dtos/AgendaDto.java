169083f9400aba98520a9cb056e3c5462e6dd265
Irene <irene.jurna@gmail.com>
2023-06-24 14:43:13 +0200

    Agenda klasse aangepast, zodat meerdere kappers een agenda kunnen aanmaken

--- src/main/java/nl/novi/kapsalon/dtos/AgendaDto.java
+++ src/main/java/nl/novi/kapsalon/dtos/AgendaDto.java
@@ -1,34 +1,61 @@
 package nl.novi.kapsalon.dtos;
 
+import jakarta.validation.constraints.AssertTrue;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AgendaDto {
     public Long id;
+
+//    public LocalDate date;
+    public List<String> workingDays;
+    public List<Integer> timeSlotsPerDay = new ArrayList<>();
     public Long hairdresserId;
-    public List<Integer> timeSlots = new ArrayList<>();
+
+    @AssertTrue(message = "De lijst met werkdagen moet even lang zijn als de lijst met tijdslots per dag")
+    public boolean isListEqualLength() {
+        return workingDays.size() == timeSlotsPerDay.size();
+    }
 
     public Long getId() {
         return id;
-    }
-
-    public Long getHairdresserId() {
-        return hairdresserId;
-    }
-
-    public List<Integer> getTimeSlots() {
-        return timeSlots;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public void setHairdresserId(Long hairdresserId) {
-        this.hairdresserId = hairdresserId;
+//    public LocalDate getDate() {
+//        return date;
+//    }
+//
+//    public void setDate(LocalDate date) {
+//        this.date = date;
+//    }
+
+    public List<String> getWorkingDays() {
+        return workingDays;
     }
 
-    public void setTimeSlots(List<Integer> timeSlots) {
-        this.timeSlots = timeSlots;
+    public void setWorkingDays(List<String> workingDays) {
+        this.workingDays = workingDays;
+    }
+
+    public List<Integer> getTimeSlotsPerDay() {
+        return timeSlotsPerDay;
+    }
+
+    public void setTimeSlotsPerDay(List<Integer> timeSlotsPerDay) {
+        this.timeSlotsPerDay = timeSlotsPerDay;
+    }
+
+    public Long getHairdresserId() {
+        return hairdresserId;
+    }
+
+    public void setHairdresserId(Long hairdresserId) {
+        this.hairdresserId = hairdresserId;
     }
 }
