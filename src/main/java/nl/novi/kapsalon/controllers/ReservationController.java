00581550264addd5e27af877196c7ecde9569329
Irene <irene.jurna@gmail.com>
2023-06-24 16:10:34 +0200

    Pogingen om een reservering te kunnen maken, mislukt

--- src/main/java/nl/novi/kapsalon/controllers/ReservationController.java
+++ src/main/java/nl/novi/kapsalon/controllers/ReservationController.java
@@ -1,18 +1,28 @@
 package nl.novi.kapsalon.controllers;
 
+import jakarta.validation.Valid;
+import nl.novi.kapsalon.dtos.ReservationDto;
 import nl.novi.kapsalon.services.AgendaService;
 import nl.novi.kapsalon.services.ReservationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("reservation")
 public class ReservationController {
     private final ReservationService reservationService;
-    private final AgendaService agendaService;
 
-    public ReservationController(ReservationService reservationService, AgendaService agendaService) {
+    public ReservationController(ReservationService reservationService) {
         this.reservationService = reservationService;
-        this.agendaService = agendaService;
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_CUSTOMER', 'ROLE_HAIRDRESSER', 'ROLE_OWNER')")
+    @PostMapping("")
+    public ResponseEntity<Object> makeReservation(@Valid @RequestBody ReservationDto rDto) {
+        reservationService.makeReservation(rDto);
+        return new ResponseEntity<>(rDto, HttpStatus.CREATED);
     }
 
 }
